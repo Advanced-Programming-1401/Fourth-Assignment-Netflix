24d8ae321d43a511b663db0283a54947be4012b4
Retriever5500 <Retriever5500@gmail.com>
2023-03-15 17:56:38 +0330

    Implememt Movie class

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -3,11 +3,19 @@ package org.example;
 import java.util.ArrayList;
 
 class Movie extends TVShow {
-    /*
-     *Movie is extended from TVShow and has extra attribute length.
-     */
-    public Movie()
+    private int length;
+
+    public Movie(String title, String genre, int year, int duration, float rating, ArrayList<String> cast, int length)
     {
-        super();
+        super(title, genre, year, duration, rating, cast);
+        this.length = length;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
     }
 }
