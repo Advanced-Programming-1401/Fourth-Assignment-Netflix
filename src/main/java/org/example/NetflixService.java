f417c11cf8a9e5ec89cb5453cc20f7d64d59aa05
Retriever5500 <Retriever5500@gmail.com>
2023-03-15 17:57:01 +0330

    Implement NetflixService class

--- src/main/java/org/example/NetflixService.java
+++ src/main/java/org/example/NetflixService.java
@@ -3,47 +3,126 @@ package org.example;
 import java.util.ArrayList;
 
 class NetflixService {
-    /*
-     *The NetflixService should have an Arraylist of users, tv shows and movies.
-     *The NetflixService should have a User object which represents current user.
-     */
+    private ArrayList<User> users;
+    private ArrayList<TVShow> shows;
+    private ArrayList<Movie> movies;
+
+    private User currentUser;
+
+    public NetflixService() {
+        this.users = new ArrayList<User>();
+        this.shows = new ArrayList<TVShow>();
+        this.movies = new ArrayList<Movie>();
+        this.currentUser = null;
+    }
+
+    public User getCurrentUser() {
+        return this.currentUser;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
 
     public void addTVShow(TVShow tvShow){
-        // Implement add tv show logic here
+        shows.add(tvShow);
     }
 
     public void addMovie(Movie movie){
-        // Implement add movie logic here
+        shows.add(movie);
     }
 
-    public void createAccount(String username, String password) {
-        // Implement create account logic here
+    public boolean createAccount(String username, String password) {
+        for(User user : users) {
+            if(user.getUsername().equals(username)) {
+                return false;
+            }
+        }
+
+        User newUser = new User(username, password);
+        users.add(newUser);
+        return true;
     }
 
     public boolean login(String username, String password) {
-        // Implement login logic here
+        for(User user : users) {
+            if(user.getUsername().equals(username)) {
+                if(user.getPassword().equals(password)) {
+                    this.currentUser = user;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
         return false;
     }
 
     public void logout() {
-        // Implement logout logic here
+        this.currentUser = null;
     }
 
     public ArrayList<TVShow> searchByTitle(String title) {
-        // Implement search by title logic here
-        return null;
+        ArrayList<TVShow> resultedShows = new ArrayList<TVShow>();
+        for(TVShow show : shows) {
+            if(show.getTitle().contains(title)) {
+                resultedShows.add(show);
+            }
+        }
+
+        for(Movie movie : movies) {
+            if(movie.getTitle().contains(title)) {
+                resultedShows.add(movie);
+            }
+        }
+
+        if(resultedShows.size() == 0) {
+            return null;
+        } else {
+            return resultedShows;
+        }
     }
 
     public ArrayList<TVShow> searchByGenre(String genre) {
-        // Implement search by genre logic here
-        return null;
+        ArrayList<TVShow> resultedShows = new ArrayList<TVShow>();
+        for(TVShow show : shows) {
+            if(show.getGenre().equals(genre)) {
+                resultedShows.add(show);
+            }
+        }
+
+        for(Movie movie : movies) {
+            if(movie.getGenre().equals(genre)) {
+                resultedShows.add(movie);
+            }
+        }
+
+        if(resultedShows.size() == 0) {
+            return null;
+        } else {
+            return resultedShows;
+        }
     }
 
     public ArrayList<TVShow> searchByReleaseYear(int year) {
-        // Implement search by release year logic here
-        return null;
+        ArrayList<TVShow> resultedShows = new ArrayList<TVShow>();
+        for(TVShow show : shows) {
+            if(show.getYear() == year) {
+                resultedShows.add(show);
+            }
+        }
+
+        for(Movie movie : movies) {
+            if(movie.getYear() == year) {
+                resultedShows.add(movie);
+            }
+        }
+
+        if(resultedShows.size() == 0) {
+            return null;
+        } else {
+            return resultedShows;
+        }
     }
-
-
 }
 
